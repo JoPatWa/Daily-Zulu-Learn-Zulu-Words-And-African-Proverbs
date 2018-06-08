29a3f9b19a8837e6fecc8a5719e9c36631d48b0f
Jonathan Ward <camon88@gmail.com>
2018-06-08 00:07:55 -0400

    Deleted config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-const config = {};
-
-config.region = "us-east-1",
-config.accessKeyId = "AKIAIZ75KSNFU7FB7ZPA",
-config.secretAccessKey = "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"
-config.bucketName = "daily-zulu-flash-briefing"
-
-module.exports = config;
